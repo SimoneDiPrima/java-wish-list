6760ac580c4f4b0934e305312dfc680c1fbb8450
SimoneDiPrima <simonediprimagenova@gmail.com>
2022-11-23 16:35:29 +0100

    second commit

--- src/org/generation/italy/christmas/Main2Parte.java
+++ src/org/generation/italy/christmas/Main2Parte.java
@@ -2,7 +2,9 @@ package org.generation.italy.christmas;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main2Parte {
@@ -16,6 +18,22 @@ public class Main2Parte {
 			letters.add(word);
 		}
 		System.out.print(letters);
+		
+		System.out.print("\n------------------------------------");
+		
+		Scanner sc2 = new Scanner(System.in);
+		System.out.print("dimmi una parola:");
+		String word2 = sc.nextLine();
+		Map<Integer,String> map = new HashMap<>();
+		
+		for(Integer key: map.keySet()) {
+			
+		map.put(5, word2);
+		System.out.println(map);
+		
+		String value = map.get(key);
+		
+		System.out.println(key + " --> " + value);
 	}
-
+	}
 }
