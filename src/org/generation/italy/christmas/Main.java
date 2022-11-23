e413b007f41a5e9ab7870f9abe2effeeba3f5945
SimoneDiPrima <simonediprimagenova@gmail.com>
2022-11-23 14:53:12 +0100

    second commit

--- src/org/generation/italy/christmas/Main.java
+++ src/org/generation/italy/christmas/Main.java
@@ -3,12 +3,24 @@ package org.generation.italy.christmas;
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 
 
 public class Main {
-
+	
+	public static class StringComparator implements Comparator<String>{
+		@Override public int compare(String o1,String o2) {
+			if(o1.length() > o2.length()) {
+				return 1;
+			}
+			else if(o1.length() > o2.length()) {
+				return -1;
+			}
+			return 0;
+	}
+		}
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
@@ -30,6 +42,8 @@ public class Main {
 		
 		Collections.sort(gifts);
 		System.out.println(gifts);
+		System.out.print("----------------------------");
+		Collections.frequency(gifts, "aeiou");
 		
 	}
 
